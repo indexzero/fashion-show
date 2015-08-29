8c24937504790e8f6f4c61ceec548c55e19dc493
indexzero <charlie.robbins@gmail.com>
2015-08-28 20:15:26 -0700

    [test fix] Small fixes and tests (mostly stubs) for lib/defaultify.js

--- test/unit/defaultify.test.js
+++ test/unit/defaultify.test.js
@@ -1,7 +1,36 @@
 'use strict';
 
-var assume = require('assume');
+var assume = require('assume'),
+    defaultify = require('../../lib/defaultify');
 
 describe('defaultify (unit)', function () {
+  it('should have expected defaults', function () {
+    var defaults = defaultify();
+    assume(defaults.commands).deep.equals(['jscs', 'eslint']);
+    assume(defaults.targets).deep.equals(['test/unit/defaultify.test.js']);
+    assume(defaults.rc).equals(undefined);
+    assume(defaults.fix).equals(undefined);
+    assume(defaults.reporter).equals(undefined);
+    assume(defaults.global).equals(undefined);
+    assume(defaults.exts).deep.equals(['.js']);
+  });
 
+  it('--rc');
+  it('--fix');
+  it('--reporter');
+  it('--global');
+  it('--command (one)');
+  it('--command (multiple)');
+  it('--ext (one)');
+  it('--ext (multiple)');
+  it('argv._ (one)');
+  it('argv._ (multiple)');
+
+  it('{ commands }');
+  it('{ targets }');
+  it('{ rc }');
+  it('{ fix }');
+  it('{ reporter }');
+  it('{ global }');
+  it('{ exts }');
 });
